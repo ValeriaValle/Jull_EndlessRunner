3a37776be00176264fe9622aea1c21fb89d248df
Valeria Valle <valeria.valle.96@gmail.com>
2019-09-07 17:50:37 -0500

    Player Slide Test

--- RunnerGameJam/Assets/Scripts/CharacterBehaviour.cs
+++ RunnerGameJam/Assets/Scripts/CharacterBehaviour.cs
@@ -8,17 +8,25 @@ public class CharacterBehaviour : MonoBehaviour
     private float jumpThrust;
     private bool grounded;
 
+    [SerializeField]
+    private Vector2 colSizeSmall;
+    private Vector2 colSizeNormal;
     private BoxCollider2D col;
+    private bool isSliding;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponentInChildren<Rigidbody2D>();
+        col = GetComponentInChildren<BoxCollider2D>();
+        colSizeNormal = col.size;
         grounded = true;
+        isSliding = false;
     }
 
     void Update()
     {
         Jump();
+        Slide();
     }
 
     void OnCollisionStay2D(Collision2D other)
@@ -45,12 +53,18 @@ public class CharacterBehaviour : MonoBehaviour
         }
     }
 
-    private void Duck()
+    private void Slide()
     {
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && !isSliding)
         {
-            col.size = new Vector3();
+            col.size = colSizeSmall;
+            isSliding = true;
+            //transform.localScale = new Vector3();
         }
-
+        if (Input.GetKeyUp("space") && isSliding)
+        {
+            col.size = colSizeNormal;
+            isSliding = false;
+        }
     }
 }
