3d3fd90c9e991137ebb778c50c541b1cb4fe6dcf
Valeria Valle <valeria.valle.96@gmail.com>
2019-09-07 15:14:27 -0500

    Base Terrain Movement 2

---
+++ RunnerGameJam/Assets/Scripts/InitialTerrainMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityTools.ScriptableVariables;
+
+public class InitialTerrainMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GenericFloat terrainMoveSpeed;
+
+    [SerializeField]
+    private float limitToDestroy;
+
+    void Update()
+    {
+        float step = Time.deltaTime * terrainMoveSpeed.var;
+        transform.Translate(Vector3.left * terrainMoveSpeed.var);
+
+        if (transform.position.x <= limitToDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
