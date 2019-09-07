9ef34fcef16451e27f7ab6732e0987cea57643ac
Valeria Valle <valeria.valle.96@gmail.com>
2019-09-07 15:48:30 -0500

    Player Jump

---
+++ RunnerGameJam/Assets/Scripts/CharacterBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField]
+    private float thrust;
+
+    private bool grounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        grounded = true;
+    }
+
+    void Update()
+    {
+        Jump();
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Terrain"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Terrain"))
+        {
+            grounded = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown("space") && grounded)
+        {
+            rb.AddForce(transform.up * thrust);
+        }
+    }
+}
